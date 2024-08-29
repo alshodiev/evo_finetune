c8c3837e2d814dbeb4f4058c7f336b891e79bd10
alshodiev <alshodiev01@gmail.com>
2024-08-29 13:50:21 -0400

    final commit

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -1,24 +1,11 @@
 from itertools import product
-import torch.optim as optim
-
 
 def hyperparameter_grid():
     param_grid = {
-        #'optimizer': ['AdamW'],  # Example optimizers 
-        'num_epochs': [2],
-        'batch_size': [16], # maybe size=8 
-        'accumulation_steps': [2],
-        'learning_rate': [0.001]
+        'num_epochs': [1],  
+        'batch_size': [16], 
+        'learning_rate': [0.001] 
     }
-    '''
-    param_grid = {
-        'num_epochs': [10, 20, 30],
-        'batch_size': [16, 32, 64],
-        'accumulation_steps': [2, 4, 8],
-        'learning_rate': [0.001, 0.0005, 0.0001]
-    }
-    '''
+
     for params in product(*param_grid.values()):
         yield dict(zip(param_grid.keys(), params))
-
-        
